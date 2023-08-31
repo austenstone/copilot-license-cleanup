be04e808aa4739fd3839f2a6be4da21cf2c2e3b8
Austen Stone <austenstone@github.com>
2023-08-31 06:28:23 -0400

    test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -3,8 +3,20 @@ import * as cp from 'child_process';
 import * as path from 'path';
 import { test } from '@jest/globals';
 
+const addInput = (key, value) => {
+  process.env[`INPUT_${key.replace(/ /g, '-').toUpperCase()}`] = value;
+}
+
+const input: any = {
+  'github-token': process.env.GITHUB_TOKEN,
+  organization: process.env.ORGANIZATION || 'octoaustenstone',
+  'inactive-days': process.env.INACTIVE_DAYS || '30',
+  remove: process.env.REMOVE || true,
+}
+
 test('test run', () => {
-  process.env['INPUT_GITHUB-TOKEN'] = process.env.GITHUB_TOKEN;
+  Object.entries(input).forEach(([key, value]) => addInput(key, value));
+  process.env['GITHUB_REPOSITORY'] = 'austenstone/copilot-license-cleanup';
   const np = process.execPath;
   const ip = path.join(__dirname, '..', 'dist', 'index.js');
   const options: cp.ExecFileSyncOptions = {
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import momemt from 'moment';
 
 interface Input {
   token: string;
   org: string;
+  removeInactive: boolean;
+  inactiveDays: number;
 }
 
 export function getInputs(): Input {
   const result = {} as Input;
   result.token = core.getInput('github-token');
   result.org = core.getInput('organization');
+  result.removeInactive = core.getBooleanInput('remove');
+  result.inactiveDays = parseInt(core.getInput('inactive-days'));
   return result;
 }
 
@@ -34,8 +39,19 @@ const run = async (): Promise<void> => {
     const lastActive = new Date(seat.last_activity_at);
     const diff = now.getTime() - lastActive.getTime();
     const diffDays = Math.ceil(diff / (1000 * 3600 * 24));
-    return diffDays > 30;
+    return diffDays > input.inactiveDays;
   }).sort((a, b) => (a.last_activity_at === null ? -1 : new Date(a.last_activity_at).getTime() - new Date(b.last_activity_at).getTime()));
+
+  core.setOutput('inactive-seats', JSON.stringify(inactiveSeats));
+  core.setOutput('inactive-seat-count', inactiveSeats.length.toString());
+  core.setOutput('seat-count', seats.length.toString());
+
+  if (input.removeInactive) {
+    await octokit.request(`DELETE /orgs/{org}/copilot/billing/selected_users`, {
+      org: input.org,
+      selected_usernames: inactiveSeats.map(seat => seat.assignee.login),
+    });
+  }
 
   await core.summary
     .addHeading("Inactive Seats")
@@ -47,7 +63,7 @@ const run = async (): Promise<void> => {
       ],
       ...inactiveSeats.map(seat => [
         seat.assignee.login || '????',
-        seat.last_activity_at || 'Never'
+        momemt(seat.last_activity_at) || 'Never'
       ])
     ])
     .addLink('View GitHub Copilot seats!', `https://github.com/organizations/${github.context.repo.owner}/settings/copilot/seat_management`)
